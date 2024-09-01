4347134d9ffd2a5390a4fff3527a1c979ae01cdc
RamiTaleb27 <rami.m.taleb@gmail.com>
2024-09-01 11:54:40 +0300

    updated

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -5,7 +5,7 @@ import TanatousStudio from '../assets/images/—Pngtree—design graphic tool so
 import virtualR from '../assets/images/VR_Logo_Virtual_Reality_Logo.svg.png'
 export const ProjectCard = () => {
     const projectsCards = [
-        {src:leon, title:"Leon" , desc:" Super Creative Agency Web Template" , href:"https://ramitaleb27.github.io/Leon-Landing-Page/",alt:"Leon pic"},
+        {src:leon, title:"Leon" , desc:"Creative Agency Web Template" , href:"https://ramitaleb27.github.io/Leon-Landing-Page/",alt:"Leon pic"},
         {src:TanatousStudio, title:"TanatousStudio" , desc:"Design Company" , href:"https://ramitaleb27.github.io/TanatousStudio-LandingPage/",alt:"Studio pic"},
         {src:virtualR, title:"VirtualR" , desc:"Virual Reality app company" , href:"https://main--virtualr-app.netlify.app/",alt:"Virtual pic"}
     ];
@@ -17,7 +17,7 @@ export const ProjectCard = () => {
                 <a className={styles.project_card} href={item.href} target = "_blank" key={index}>
                     <img className={styles.hover} src={item.src} alt={item.alt} />
                     <h3>{item.title}</h3>
-                    <p>{item.desc}</p>
+                    <p className={styles.projectsDesc}>{item.desc}</p>
                 </a>
             )
         })
